312a78bea2f31b0f859500ec6fe2a8ca6a90fac6
somnath <somnathnath8482@gmail.com>
2023-06-29 21:19:17 +0530

    smn

--- app/src/main/java/easysent/in/Activity/ForgotPasswordActivity.java
+++ app/src/main/java/easysent/in/Activity/ForgotPasswordActivity.java
@@ -4,15 +4,82 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+import easysent.in.Helper.Constants;
+import easysent.in.Helper.MethodClass;
+import easysent.in.Interface.Response;
 import easysent.in.R;
+import easysent.in.Response.Register.RegisterResponse;
+import easysent.in.databinding.ActivityForgotPasswordBinding;
+import io.github.muddz.styleabletoast.StyleableToast;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
-
+    ActivityForgotPasswordBinding binding;
+    Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forgot_password);
-        findViewById(R.id.btn_sendOtp).setOnClickListener(view -> startActivity(new Intent(this,OtpActivity.class)));
+        binding = ActivityForgotPasswordBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Init();
     }
+    private void Init() {
+        binding.btnSendOtp.setOnClickListener(view -> {
+            //  startActivity(new Intent(this,OtpActivity.class));
+
+
+            HashMap<String, Object> map = new HashMap<>();
+            try {
+                map.put("email", MethodClass.CheckEmpty(binding.edEmail));
+
+            } catch (Exception e) {
+                EditText editText = binding.getRoot().findViewById(Integer.parseInt(e.getMessage()));
+                editText.requestFocus();
+                e.printStackTrace();
+                return;
+            }
+            Send(map);
+        });
+
+        binding.btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), SignupActivity.class));
+            }
+        });
+    }
+
+    private void Send(HashMap<String, Object> map) {
+        MethodClass.Call(new Response() {
+            @Override
+            public void onResponse(JSONObject res) {
+                if (res != null) {
+                    Gson json = new Gson();
+
+                    RegisterResponse registerResponse = json.fromJson(res.toString(), RegisterResponse.class);
+
+                    if (registerResponse!=null){
+                        StyleableToast.makeText(getApplicationContext(), registerResponse.getSuccess(), Toast.LENGTH_SHORT, R.style.mytoast).show();
+                        Intent intent   = new Intent(getApplicationContext(),OtpActivity.class);
+                        intent.putExtra("otp", registerResponse.getOtp());
+                        intent.putExtra("from", "forgot");
+                        intent.putExtra("email", binding.edEmail.getText().toString().trim());
+                        startActivity(intent);
+                    }
+                }
+            }
+        }, Constants.BASE_URL + Constants.FORGOT_PASSWORD, ForgotPasswordActivity.this, handler, map);
+
+    }
+
 }
--- app/src/main/java/easysent/in/Activity/SetPasswordActivity.java
+++ app/src/main/java/easysent/in/Activity/SetPasswordActivity.java
@@ -2,15 +2,94 @@ package easysent.in.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+import easysent.in.Helper.Constants;
+import easysent.in.Helper.MethodClass;
+import easysent.in.Interface.Response;
 import easysent.in.R;
+import easysent.in.databinding.ActivitySetPasswordBinding;
+import io.github.muddz.styleabletoast.StyleableToast;
 
 public class SetPasswordActivity extends AppCompatActivity {
-
+ActivitySetPasswordBinding binding;
+Handler handler = new Handler();
+    String email = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_set_password);
+        binding = ActivitySetPasswordBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        if (getIntent() != null) {
+            email = getIntent().getStringExtra("email");
+        }
+        Init();
+
+    }
+
+    private void Init() {
+        binding.btnSubmit.setOnClickListener(view -> {
+
+            if (Validate()) {
+                HashMap<String, Object> map = new HashMap<>();
+                map.put("password", binding.password.getText().toString().trim());
+                map.put("email", email);
+                Submit(map);
+            }
+
+        });
+    }
+
+    private void Submit(HashMap<String, Object> map) {
+
+        MethodClass.Call(new Response() {
+            @Override
+            public void onResponse(JSONObject res) {
+                try {
+                    if (res != null) {
+                        StyleableToast.makeText(getApplicationContext(), res.getString("success"), Toast.LENGTH_SHORT, R.style.mytoast).show();
+                        startActivity(new Intent(SetPasswordActivity.this,LoginActivity.class));
+                        finishAffinity();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(SetPasswordActivity.this, "Something Went Wrong", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        }, Constants.BASE_URL + Constants.SET_PASSWORD, SetPasswordActivity.this,handler, map);
+    }
+
+    private boolean Validate() {
+        if (!MethodClass.checkEditText(binding.password)) {
+            binding.password.setError("Please Enter Password");
+            binding.password.requestFocus();
+            return false;
+        } else if (binding.password.getText().toString().trim().length() < 8) {
+            binding.password.setError("Password Must be 8 Character Long");
+            binding.password.requestFocus();
+            return false;
+
+        }
+
+        if (!MethodClass.checkEditText(binding.conformPassword)) {
+            binding.conformPassword.setError("Please  Re Enter Your Password");
+            binding.conformPassword.requestFocus();
+            return false;
+        } else if (!binding.password.getText().toString().trim().equals(binding.conformPassword.getText().toString().trim())) {
+            binding.conformPassword.setError("Conform Password Must Same as New Password");
+            binding.conformPassword.requestFocus();
+            return false;
+        }
+        return true;
     }
 }
